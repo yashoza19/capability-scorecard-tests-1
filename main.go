773bbbb93c1b421051c317c78531ada9aa876543
Melvin Hillsman <mhillsma@redhat.com>
2022-03-01 08:45:56 -0600

    Add working placeholder capability tests
    Signed-off-by: Melvin Hillsman <mhillsma@redhat.com>

--- main.go
+++ main.go
@@ -19,6 +19,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
 
 	scapiv1alpha3 "github.com/operator-framework/api/pkg/apis/scorecard/v1alpha3"
 	apimanifests "github.com/operator-framework/api/pkg/manifests"
@@ -33,7 +34,7 @@ import (
 // this binary to run various tests all from within a single
 // test image.
 
-const PodBundleRoot = "bundle/manifests"
+const PodBundleRoot = "/bundle"
 
 func main() {
 	entrypoint := os.Args[1:]
@@ -52,10 +53,16 @@ func main() {
 	// Names of the custom tests which would be passed in the
 	// `operator-sdk` command.
 	switch entrypoint[0] {
-	case CustomTest1Name:
-		result = CustomTest1(cfg)
-	case CustomTest2Name:
-		result = CustomTest2(cfg)
+	case CapabilityLevelOneTestName:
+		result = CapabilityLevelOneTest(cfg)
+	case CapabilityLevelTwoTestName:
+		result = CapabilityLevelTwoTest(cfg)
+	case CapabilityLevelThreeTestName:
+		result = CapabilityLevelThreeTest(cfg)
+	case CapabilityLevelFourTestName:
+		result = CapabilityLevelFourTest(cfg)
+	case CapabilityLevelFiveTestName:
+		result = CapabilityLevelFiveTest(cfg)
 	default:
 		result = printValidTests()
 	}
@@ -76,9 +83,13 @@ func printValidTests() scapiv1alpha3.TestStatus {
 	result.Errors = make([]string, 0)
 	result.Suggestions = make([]string, 0)
 
-	str := fmt.Sprintf("Valid tests for this image include: %s %s",
-		CustomTest1Name,
-		CustomTest2Name)
+	str := fmt.Sprintf("Valid tests for this image include: %s %s %s %s %s",
+		CapabilityLevelOneTestName,
+		CapabilityLevelTwoTestName,
+		CapabilityLevelThreeTestName,
+		CapabilityLevelFourTestName,
+		CapabilityLevelFiveTestName,
+		)
 	result.Errors = append(result.Errors, str)
 	return scapiv1alpha3.TestStatus{
 		Results: []scapiv1alpha3.TestResult{result},
@@ -86,38 +97,170 @@ func printValidTests() scapiv1alpha3.TestStatus {
 }
 
 const (
-	CustomTest1Name = "customtest1"
-	CustomTest2Name = "customtest2"
+	CapabilityLevelOneTestName = "capability-level-one"
+	CapabilityLevelTwoTestName = "capability-level-two"
+	CapabilityLevelThreeTestName = "capability-level-three"
+	CapabilityLevelFourTestName = "capability-level-four"
+	CapabilityLevelFiveTestName = "capability-level-five"
 )
 
-// Define any operator specific custom tests here.
-// CustomTest1 and CustomTest2 are example test functions. Relevant operator specific
-// test logic is to be implemented in similarly.
+// CapabilityLevelOneTest accepts a *apimanifests.Bundle and returns scapiv1alpha3.TestStatus
+// This test verifies operator capability level one has been met.
 
-func CustomTest1(bundle *apimanifests.Bundle) scapiv1alpha3.TestStatus {
+func CapabilityLevelOneTest(bundle *apimanifests.Bundle) scapiv1alpha3.TestStatus {
 	r := scapiv1alpha3.TestResult{}
-	r.Name = CustomTest1Name
+	r.Name = CapabilityLevelOneTestName
 	r.State = scapiv1alpha3.PassState
 	r.Errors = make([]string, 0)
 	r.Suggestions = make([]string, 0)
+
+	// Install operator
+	output, err := exec.Command("/usr/local/bin/operator-sdk", "version").Output()
+	if err != nil {
+		log.Fatalf("Unable to run operator-sdk: %v", err)
+	}
+
+	log.Println(string(output))
+
+	// Create CR from first object in alm-examples array
+
+	// Get alm-examples
 	almExamples := bundle.CSV.GetAnnotations()["alm-examples"]
+	// TODO: should this be a fatal error?
 	if almExamples == "" {
 		fmt.Println("no alm-examples in the bundle CSV")
 	}
+
+	// Apply alm-example to cluster
 
 	return wrapResult(r)
 }
 
-func CustomTest2(bundle *apimanifests.Bundle) scapiv1alpha3.TestStatus {
+// CapabilityLevelTwoTest accepts a *apimanifests.Bundle and returns scapiv1alpha3.TestStatus
+// This test verifies operator capability level two has been met.
+
+func CapabilityLevelTwoTest(bundle *apimanifests.Bundle) scapiv1alpha3.TestStatus {
 	r := scapiv1alpha3.TestResult{}
-	r.Name = CustomTest2Name
+	r.Name = CapabilityLevelTwoTestName
 	r.State = scapiv1alpha3.PassState
 	r.Errors = make([]string, 0)
 	r.Suggestions = make([]string, 0)
+
+	// Install operator
+	output, err := exec.Command("/usr/local/bin/operator-sdk", "version").Output()
+	if err != nil {
+		log.Fatalf("Unable to run operator-sdk: %v", err)
+	}
+
+	log.Println(string(output))
+
+	// Create CR from first object in alm-examples array
+
+	// Get alm-examples
 	almExamples := bundle.CSV.GetAnnotations()["alm-examples"]
+	// TODO: should this be a fatal error?
 	if almExamples == "" {
 		fmt.Println("no alm-examples in the bundle CSV")
 	}
+
+	// Apply alm-example to cluster
+
+	return wrapResult(r)
+}
+
+// CapabilityLevelThreeTest accepts a *apimanifests.Bundle and returns scapiv1alpha3.TestStatus
+// This test verifies operator capability level three has been met.
+
+func CapabilityLevelThreeTest(bundle *apimanifests.Bundle) scapiv1alpha3.TestStatus {
+	r := scapiv1alpha3.TestResult{}
+	r.Name = CapabilityLevelThreeTestName
+	r.State = scapiv1alpha3.PassState
+	r.Errors = make([]string, 0)
+	r.Suggestions = make([]string, 0)
+
+	// Install operator
+	output, err := exec.Command("/usr/local/bin/operator-sdk", "version").Output()
+	if err != nil {
+		log.Fatalf("Unable to run operator-sdk: %v", err)
+	}
+
+	log.Println(string(output))
+
+	// Create CR from first object in alm-examples array
+
+	// Get alm-examples
+	almExamples := bundle.CSV.GetAnnotations()["alm-examples"]
+	// TODO: should this be a fatal error?
+	if almExamples == "" {
+		fmt.Println("no alm-examples in the bundle CSV")
+	}
+
+	// Apply alm-example to cluster
+
+	return wrapResult(r)
+}
+
+// CapabilityLevelFourTest accepts a *apimanifests.Bundle and returns scapiv1alpha3.TestStatus
+// This test verifies operator capability level four has been met.
+
+func CapabilityLevelFourTest(bundle *apimanifests.Bundle) scapiv1alpha3.TestStatus {
+	r := scapiv1alpha3.TestResult{}
+	r.Name = CapabilityLevelFourTestName
+	r.State = scapiv1alpha3.PassState
+	r.Errors = make([]string, 0)
+	r.Suggestions = make([]string, 0)
+
+	// Install operator
+	output, err := exec.Command("/usr/local/bin/operator-sdk", "version").Output()
+	if err != nil {
+		log.Fatalf("Unable to run operator-sdk: %v", err)
+	}
+
+	log.Println(string(output))
+
+	// Create CR from first object in alm-examples array
+
+	// Get alm-examples
+	almExamples := bundle.CSV.GetAnnotations()["alm-examples"]
+	// TODO: should this be a fatal error?
+	if almExamples == "" {
+		fmt.Println("no alm-examples in the bundle CSV")
+	}
+
+	// Apply alm-example to cluster
+
+	return wrapResult(r)
+}
+
+// CapabilityLevelFiveTest accepts a *apimanifests.Bundle and returns scapiv1alpha3.TestStatus
+// This test verifies operator capability level five has been met.
+
+func CapabilityLevelFiveTest(bundle *apimanifests.Bundle) scapiv1alpha3.TestStatus {
+	r := scapiv1alpha3.TestResult{}
+	r.Name = CapabilityLevelFiveTestName
+	r.State = scapiv1alpha3.PassState
+	r.Errors = make([]string, 0)
+	r.Suggestions = make([]string, 0)
+
+	// Install operator
+	output, err := exec.Command("/usr/local/bin/operator-sdk", "version").Output()
+	if err != nil {
+		log.Fatalf("Unable to run operator-sdk: %v", err)
+	}
+
+	log.Println(string(output))
+
+	// Create CR from first object in alm-examples array
+
+	// Get alm-examples
+	almExamples := bundle.CSV.GetAnnotations()["alm-examples"]
+	// TODO: should this be a fatal error?
+	if almExamples == "" {
+		fmt.Println("no alm-examples in the bundle CSV")
+	}
+
+	// Apply alm-example to cluster
+
 	return wrapResult(r)
 }
 
